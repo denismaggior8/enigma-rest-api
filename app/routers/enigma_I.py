86f69f1ffc99e7036adf2cb27c375c2464bbdaca
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2024-04-30 08:58:22 +0200

    refactoring

--- app/routers/enigma_I.py
+++ app/routers/enigma_I.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from ..models.enigma_I_models import EnigmaIRequest, EnigmaIResponse
 from enigmapython.PlugboardPassthrough import PlugboardPassthrough
-from enigmapython.Utils import Utils
 from enigmapython.Rotor import Rotor
 from enigmapython.EtwPassthrough import EtwPassthrough
 from enigmapython.EnigmaI import EnigmaI
@@ -20,10 +19,23 @@ router = APIRouter(
 @router.post("/encrypt")
 async def encrypt(request: EnigmaIRequest) -> EnigmaIResponse:
     plugboard = PlugboardPassthrough()
-    rotor1Class = Utils.get_class_instance(Rotor.lookup["I_"+request.rotors[0].type].__module__+"."+Rotor.lookup["I_"+request.rotors[0].type].__name__)
-    rotor2Class = Utils.get_class_instance(Rotor.lookup["I_"+request.rotors[1].type].__module__+"."+Rotor.lookup["I_"+request.rotors[1].type].__name__)
-    rotor3Class = Utils.get_class_instance(Rotor.lookup["I_"+request.rotors[2].type].__module__+"."+Rotor.lookup["I_"+request.rotors[2].type].__name__)
-    reflectorClass = Utils.get_class_instance(Rotor.lookup[request.reflector].__module__+"."+Rotor.lookup[request.reflector].__name__)
+
+    rotor1 = Rotor.get_instance_from_tag("I_"+request.rotors[0].type)
+    rotor1.position = request.rotors[0].position
+    rotor1.ring = request.rotors[0].ring
+
+    rotor2 = Rotor.get_instance_from_tag("I_"+request.rotors[1].type)
+    rotor2.position = request.rotors[1].position
+    rotor2.ring = request.rotors[1].ring
+
+    rotor3 = Rotor.get_instance_from_tag("I_"+request.rotors[2].type)
+    rotor3.position = request.rotors[2].position
+    rotor3.ring = request.rotors[2].ring
+    
+    reflector = Rotor.get_instance_from_tag(request.reflector)
+
     etw = EtwPassthrough()
-    enigma = EnigmaI(plugboard, rotor3Class(position=request.rotors[0].position,ring=request.rotors[0].ring), rotor2Class(position=request.rotors[1].position,ring=request.rotors[1].ring), rotor1Class(position=request.rotors[2].position,ring=request.rotors[2].ring), reflectorClass(), etw, True)
+
+    enigma = EnigmaI(plugboard, rotor3, rotor2, rotor1, reflector, etw, True)
+    
     return EnigmaIResponse(cyphertext=enigma.input_string(request.cleartext.lower()).upper())
