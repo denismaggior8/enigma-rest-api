1deefc710326f4334926433f9b49ae36c9ab168e
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2024-05-01 22:26:40 +0200

    fix

--- app/routers/enigma_I.py
+++ app/routers/enigma_I.py
@@ -18,21 +18,23 @@ router = APIRouter(prefix="/{root_path}/{api_version}/enigma".format(root_path =
 
 @router.post("/{model}/encrypt")
 async def encrypt(model: str, request: EnigmaIRequest) -> EnigmaIResponse:
-    plugboard = SwappablePlugboard()
+    
 
     rotors = []
-
     for i in range(len(request.rotors)):
         rotor = Rotor.get_instance_from_tag("I_"+request.rotors[i].type)
         rotor.position = request.rotors[i].position
         rotor.ring = request.rotors[i].ring
         rotors.append(rotor)
-
     rotors.reverse()
     
     reflector = Reflector.get_instance_from_tag(request.reflector)
 
     etw = EtwPassthrough()
+
+    plugboard = SwappablePlugboard()   
+    for plugboard_wiring in request.plugboard.wirings:
+        plugboard.swap(plugboard_wiring.from_letter.lower(),plugboard_wiring.to_letter.lower())
 
     enigma = Enigma(
         plugboard = plugboard,
@@ -41,8 +43,5 @@ async def encrypt(model: str, request: EnigmaIRequest) -> EnigmaIResponse:
         etw = etw,
         auto_increment_rotors = request.auto_increment_rotors
         )
-    
-    for plugboard_wiring in request.plugboard.wirings:
-        plugboard.swap(plugboard_wiring.from_letter.lower(),plugboard_wiring.to_letter.lower())
-    
+
     return EnigmaIResponse(cyphertext=enigma.input_string(request.cleartext.lower()).upper())
