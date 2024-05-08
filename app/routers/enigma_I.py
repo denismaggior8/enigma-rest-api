0bd8d45e09530618789d3041dd97ed56b3c75720
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2024-05-08 21:32:02 +0200

    docs

--- app/routers/enigma_I.py
+++ app/routers/enigma_I.py
@@ -20,13 +20,16 @@ router = APIRouter(prefix="/{root_path}/{api_version}/enigma".format(root_path =
 async def encrypt(model: str, request: EnigmaIRequest) -> EnigmaIResponse:
     
     rotors = []
-    for i in range(len(request.rotors)):
-        rotor = Rotor.get_instance_from_tag("I_"+request.rotors[i].type)
-        rotor.position = request.rotors[i].position
-        rotor.ring = request.rotors[i].ring
+    #request.rotors.reverse()
+    #for i in range(len(request.rotors)):
+    for index,rotorConfig in enumerate(request.rotors):
+        print(rotorConfig)
+        rotor = Rotor.get_instance_from_tag("I_"+rotorConfig.type)
+        rotor.position = rotorConfig.position
+        rotor.ring = rotorConfig.ring
         rotors.append(rotor)
     rotors.reverse()
-    
+
     reflector = Reflector.get_instance_from_tag(request.reflector)
 
     etw = EtwPassthrough()
@@ -34,7 +37,7 @@ async def encrypt(model: str, request: EnigmaIRequest) -> EnigmaIResponse:
     plugboard = SwappablePlugboard()   
     for plugboard_wiring in request.plugboard.wirings:
         plugboard.swap(plugboard_wiring.from_letter.lower(),plugboard_wiring.to_letter.lower())
-
+   
     enigma = Enigma(
         plugboard = plugboard,
         rotors = rotors,
@@ -42,12 +45,13 @@ async def encrypt(model: str, request: EnigmaIRequest) -> EnigmaIResponse:
         etw = etw,
         auto_increment_rotors = request.auto_increment_rotors
         )
-    
+
     cypher_text = enigma.input_string(request.cleartext.lower()).upper()
-    
+
     new_rotors : ResponseRotorConfig = []
-    for i in range(len(enigma.rotors)):
-        responseRotorConfig = ResponseRotorConfig(position=enigma.rotors[i].position,ring=enigma.rotors[i].ring)
+    
+    for index,rotorConfig in enumerate(rotors):
+        responseRotorConfig = ResponseRotorConfig(position=enigma.rotors[index].position,ring=enigma.rotors[index].ring)
         new_rotors.append(responseRotorConfig)
     new_rotors.reverse()
 
